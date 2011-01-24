299dee67277e90b1efc4e4ad406b410e3668a38a
Joshua Seidel <jsseidel1@gmail.com>
2011-01-24 09:23:52 -0500

    cpufreq: add savagedzen governor
    * Feautures:
      - Based on the smartass governor by Erasmux
    
    * Modifications:
      - This Governor does haven't get_cpu_idle_time_jiffy and get_cpu_idle_time methods
        these methods are now placed in cpufreq.c
    
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -415,6 +415,9 @@ extern struct cpufreq_governor cpufreq_gov_elementalx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
+extern struct cpufreq_governor cpufreq_gov_SavagedZen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_SavagedZen)
 #endif
 
 
