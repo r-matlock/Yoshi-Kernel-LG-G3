7795966dca55fd3f02fffbd2626e1d13f4588be6
Peter Gregus <pete.gregus@gmail.com>
2012-10-15 07:51:22 +0200

    cpufreq: add smartassH3 governor
    * Feautures:
      - Based on SmartassV2 containing CPU freq. scaling tweaks by H3ROS
      - should bring better battery life maintaining performance
    
    * Modifications:
      - Tuned for G3
    
    Signed-off-by: Peter Gregus <pete.gregus@gmail.com>
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_badass;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
+extern struct cpufreq_governor cpufreq_gov_smartass_h3;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass_h3)
 #endif
 
 
