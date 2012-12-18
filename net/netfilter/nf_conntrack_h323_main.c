7c2f7d34b3d216d07d5e309c4c2c811fc9160cce
Dennis Rassmann <showp1984@gmail.com>
2012-12-18 03:32:54 +0100

    kernel: -O3 Fixes
    Change-Id: I7ff28be96dffdc02bfe677c7c524975fa0a331c5
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- net/netfilter/nf_conntrack_h323_main.c
+++ net/netfilter/nf_conntrack_h323_main.c
@@ -568,7 +568,7 @@ static int h245_help(struct sk_buff *skb, unsigned int protoff,
 	unsigned char *data = NULL;
 	int datalen = 0;
 	int dataoff = 0;
-	int ret;
+	int ret = 0;
 
 	/* Until there's been traffic both ways, don't look in packets. */
 	if (ctinfo != IP_CT_ESTABLISHED && ctinfo != IP_CT_ESTABLISHED_REPLY)
@@ -1120,7 +1120,7 @@ static int q931_help(struct sk_buff *skb, unsigned int protoff,
 	unsigned char *data = NULL;
 	int datalen = 0;
 	int dataoff = 0;
-	int ret;
+	int ret = 0;
 
 	/* Until there's been traffic both ways, don't look in packets. */
 	if (ctinfo != IP_CT_ESTABLISHED && ctinfo != IP_CT_ESTABLISHED_REPLY)
