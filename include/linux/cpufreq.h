1ab3899071f053caa1b234772e0bc33c6c3496f9
alucard24 <dmbaoh2@gmail.com>
2013-11-17 04:53:48 -0700

    cpufreq: add alucard governor
    [777jon: adapt deferrable delayed_work initializers]
    
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -433,6 +433,9 @@ extern struct cpufreq_governor cpufreq_gov_slim;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 
