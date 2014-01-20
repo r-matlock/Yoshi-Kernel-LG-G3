20606c3595de2c53e2815878d9d944516f8a274c
Paul Reioux <reioux@gmail.com>
2014-01-19 19:37:05 -0800

    cpufreq: add intelliactive governor
    * Modifications:
      - This Governor does haven't get_cpu_idle_time_jiffy and get_cpu_idle_time methods
        these methods are now placed in cpufreq.c
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -394,6 +394,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass_h3;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 
