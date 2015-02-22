9d8295424880765512f5bc49ce1d0474803c2ee1
flar2 <asegaert@gmail.com>
2015-02-22 01:19:08 -0500

    cpufreq: add ElementalX governor
    * Modifications:
      - This Governor does haven't get_cpu_idle_time_jiffy and get_cpu_idle_time methods
        these methods are now placed in cpufreq.c
    
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -409,6 +409,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
 extern struct cpufreq_governor cpufreq_gov_yankdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
+extern struct cpufreq_governor cpufreq_gov_elementalx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
 #endif
 
 
