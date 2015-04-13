f7c0843a2aefbcce17d5b431accfa16b6837b801
Robbie <RobbieLancaster811@gmail.com>
2015-04-12 22:16:23 -0600

    cpufreq: add slim governor
    * @SlimRoms governor
    
    * Modifications:
      - This Governor does haven't get_cpu_idle_time_jiffy and get_cpu_idle_time methods
        these methods are now placed in cpufreq.c
    
    Signed-off-by: Robbie <RobbieLancaster811@gmail.com>
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -50,7 +50,7 @@
 extern bool gpu_busy_state;
 #endif
 
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+#if defined(CONFIG_CPU_FREQ_GOV_ELEMENTALX) || defined(CONFIG_CPU_FREQ_GOV_SLIM)
 int graphics_boost = 6;
 #endif
 
@@ -219,7 +219,7 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+#if defined(CONFIG_CPU_FREQ_GOV_ELEMENTALX) || defined(CONFIG_CPU_FREQ_GOV_SLIM)
 	graphics_boost = pwr->active_pwrlevel;
 #endif
 }
