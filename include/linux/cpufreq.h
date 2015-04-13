df784fb7dbea710c52e0d737b507593d3194e5bf
Robbie <RobbieLancaster811@gmail.com>
2015-04-12 22:16:23 -0600

    cpufreq: slim: add new @SlimRoms governor
    Signed-off-by: Robbie <RobbieLancaster811@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -380,6 +380,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
 #endif
 
 
