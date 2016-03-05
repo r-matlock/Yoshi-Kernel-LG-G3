dd8b02904a1f2bc64502661df484ccc320736251
Paul Keith <javelinanddart@gmail.com>
2016-03-05 09:44:41 -0600

    cfq: Fix Slice Idle Check
    * Now it makes sure it's never 1
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -3913,7 +3913,7 @@ static int __init cfq_init(void)
 	 */
 	if (!cfq_slice_async)
 		cfq_slice_async = 1;
-	if (!cfq_slice_idle)
+	if (cfq_slice_idle)
 		cfq_slice_idle = 0;
 
 #ifdef CONFIG_CFQ_GROUP_IOSCHED
