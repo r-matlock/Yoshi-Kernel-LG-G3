bb0c11888addf9023aa67519d221f4cf58771b50
Paul Keith <javelinanddart@gmail.com>
2016-03-05 09:45:35 -0600

    block: bfq: fix slice Idle check
    * Now it makes sure it's never 1
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4175,7 +4175,7 @@ static int __init bfq_init(void)
 	/*
 	 * Can be 0 on HZ < 1000 setups.
 	 */
-	if (bfq_slice_idle == 0)
+	if (bfq_slice_idle)
 		bfq_slice_idle = 0;
 
 	if (bfq_timeout_async == 0)
