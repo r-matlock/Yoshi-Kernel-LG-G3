Cl3Kener/UBER-L

bcmdhd: reduce wakelocks
Signed-off-by: flar2 <asegaert@gmail.com>

drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -5824,10 +5824,10 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_rx_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
-				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/4);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/4);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;

r-matlock/Yoshi-Kernel-LG-G3

Change-Id: I2554b66f7ef256f2eee85675f3be225a32ffd6e0
Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -4676,7 +4676,7 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
-				msecs_to_jiffies(dhd->wakelock_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_timeout_enable)/4);
 #endif
 		dhd->wakelock_timeout_enable = 0;
 		spin_unlock_irqrestore(&dhd->wakelock_spinlock, flags);